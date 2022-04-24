835bde1e6a1391b6c85889da558b2efe77324f6f
Richard-AC <richard_abou_chaaya@brown.edu>
2022-04-24 18:22:08 -0400

    Support DW_TAG_GNU_template_template_param

--- src/main.rs
+++ src/main.rs
@@ -959,6 +959,7 @@ fn find_function_pointers_in_type<R>(dwarf: &Dwarf<R>, unit: &Unit<R>, entry: &D
                 }
 
                 else if child.entry().tag() == gimli::constants::DW_TAG_GNU_template_parameter_pack
+                  || child.entry().tag() == gimli::constants::DW_TAG_GNU_template_template_param
                   || child.entry().tag() == gimli::constants::DW_TAG_template_type_parameter {
                     continue;
                 }
@@ -968,6 +969,11 @@ fn find_function_pointers_in_type<R>(dwarf: &Dwarf<R>, unit: &Unit<R>, entry: &D
                     .unwrap() {
                         Some(v) => v,
                         None => {
+                            // REMOVE
+                            println!("Member: {} {:x?} {:x?} ", child.entry().tag(), child.entry().offset(), child.entry().offset().to_debug_info_offset(&unit.header).unwrap().0);
+                            let mut attrs = child.entry().attrs();
+                            while let Some(attr) = attrs.next().unwrap() {println!("   {}: {:?}", attr.name(), attr.value());} 
+                            //REMOVE
                             panic!("This member has no type");
                         }
                     }
